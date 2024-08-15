b6a395695c39569ab8506216294a95984605b239
Dan Sexton <danielsexton1999@gmail.com>
2024-08-14 20:56:40 -0400

    merged the dev branch in locally, fixed react import, has my changes to users & creation of admin page, not mounted yet

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,7 @@
-import React, {   createBrowserRouter,
-  createRoutesFromElements,Routes, Route } from "react-router-dom";
+import React from "react" 
+import {   createBrowserRouter, createRoutesFromElements, Routes, Route } from "react-router-dom";
 import Home from "./pages/homepage.jsx";
 import AboutUs from "./pages/aboutus.jsx";
-import { Button } from "antd"; //add an import call on your components pulled from antd (in your component.jsx)
-//adding a button to test, delete in future version
 import LeadCapture from "./components/leadCapture";
 
 // import './App.css'
@@ -24,7 +22,6 @@ function App() {
       </Routes>
 
       <>
-        <Button type="primary">This uses antd</Button>
         {/* example of how to use lead capture button or form on a page. this will be removed. import LeadCapture is needed on page */}
         <div style={{ padding: "20px" }}>
           {/* LeadCapture component used as a modal example */}
