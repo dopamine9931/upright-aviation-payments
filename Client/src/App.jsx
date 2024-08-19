269b653ab7d21c747da3683bff29e4d08be1cce0
David Chatman <Quintel14@gmail.com>
2024-08-19 18:25:35 -0400

    demo

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,31 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { Button } from "antd"; //add an import call on your components pulled from antd (in your component.jsx)
 //adding a button to test, delete in future version
-import LeadCapture from './components/leadCapture';
-
-import './App.css'
+import LeadCapture from "./components/leadCapture";
+import Header from "./components/header";
+import "./App.css";
 
 function App() {
-
-
   return (
     <>
+      <Header />
       <Button type="primary">This uses antd</Button>
       {/* example of how to use lead capture button or form on a page. this will be removed. import LeadCapture is needed on page */}
-      <div style={{ padding: '20px' }}>
-      {/* LeadCapture component used as a modal example */} 
-      <LeadCapture
-        isModal={true}
-      />
+      <div style={{ padding: "20px" }}>
+        {/* LeadCapture component used as a modal example */}
+        <LeadCapture isModal={true} />
 
-      {/* LeadCapture component used directly on the page example */}
-      <LeadCapture
-        isModal={false}
-      />
-    </div>
-    {/* end of lead capture form / button example. */}
+        {/* LeadCapture component used directly on the page example */}
+        <LeadCapture isModal={false} />
+      </div>
+      {/* end of lead capture form / button example. */}
     </>
   );
 }
 
-export default App
+export default App;
