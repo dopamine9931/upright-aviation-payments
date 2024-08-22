4ef36ac37c5270997aa7a2a785ce4276a7cb4e66
lucemery <lucemery316@gmail.com>
2024-08-22 17:36:15 -0500

    personal branch save

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AboutUs from "./pages/aboutus.jsx";
 import MerchantServices from "./pages/merchantservices.jsx";
 import Gateway from "./pages/gateway.jsx";
@@ -14,11 +10,13 @@ import ContactUs from "./pages/constactus.jsx";
 import { ApiKeyProvider } from "./context/apiKeyContext.jsx";
 import Homepage from "./pages/homepage.jsx";
 
-import './App.css'
-
+import "./App.css";
+import { ConfigProvider } from "antd";
+import AdminPage from "./components/adminPage.jsx";
 
 function App() {
   return (
+<<<<<<< HEAD
   <ApiKeyProvider>
     <Router>
       <Routes>
@@ -27,6 +25,33 @@ function App() {
       </Routes>
   </Router>
 </ApiKeyProvider>
+=======
+    <ApiKeyProvider>
+      <ConfigProvider
+        theme={{
+          token: {
+            colorPrimary: "#787878",
+            colorBgContainer: "#141414",
+            colorText: "#f3f3f3",
+          },
+        }}
+      >
+        <Router>
+          <Routes>
+            <Route path="/" element={<Homepage />} />
+            <Route path="/merchantservices" element={<MerchantServices />} />
+            <Route path="/gateway" element={<Gateway />} />
+            <Route path="/fuelcard" element={<FuelCardSolutions />} />
+            <Route path="/atmsolutions" element={<ATMSolutions />} />
+            <Route path="/achsolutions" element={<ACHSolutions />} />
+            <Route path="/contactus" element={<ContactUs />} />
+            <Route path="/aboutus" element={<AboutUs />} />
+            <Route path="/admin" element={<AdminPage />} />
+          </Routes>
+        </Router>
+      </ConfigProvider>
+    </ApiKeyProvider>
+>>>>>>> c2c6ec95937c4558049d0e184b2547a73b2b878d
   );
 }
 
