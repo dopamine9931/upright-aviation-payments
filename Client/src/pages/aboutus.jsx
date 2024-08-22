c0aa070bf2d7f960030e003e68b785227ee298f3
mbanyea <mbanyea@gmail.com>
2024-08-22 18:56:26 -0400

    about us

--- Client/src/pages/aboutus.jsx
+++ Client/src/pages/aboutus.jsx
@@ -1,35 +1,13 @@
 import React, { Routes, Route } from "react";
 // import React from "react";
 import { Card } from "antd";
+import AboutUsHero from "../components/aboutus/aboutusHero";
 
 // const { Meta } = Card;
 
 function AboutUs () {
   return (
-    <div>
-      <h1> About Aviation Payments </h1>
-
-      <div>
-        <Card
-          hoverable
-          style={{
-            width: 240,
-          }}
-          cover={
-            <img
-              alt="example"
-              src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"
-            />
-          }
-        >
-          {/* <Meta title="Europe Street beat" description="www.instagram.com" /> */}
-        </Card>
-      </div>
-
-      <div>
-        <h2>Our Team</h2>
-      </div>
-    </div>
+    <AboutUsHero />
   );
 };
 
