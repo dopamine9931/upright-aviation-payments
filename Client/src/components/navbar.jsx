2a802d04f4d695cc2d78ab56aceee529512d2786
Dan Sexton <166451276+dopamine9931@users.noreply.github.com>
2024-08-28 19:54:57 -0400

    Update navbar.jsx

--- Client/src/components/navbar.jsx
+++ Client/src/components/navbar.jsx
@@ -1,9 +1,9 @@
-<<<<<<< HEAD
+
 import React, { useState } from "react";
 import { Menu, Drawer } from "antd";
 import { MenuOutlined } from "@ant-design/icons";
 
-function App() {
+function Navbar() {
   const [openMENU, setOpenMENU] = useState(false);
 
   return (
@@ -63,7 +63,7 @@ function AppMenu({ isInline = false }) {
   );
 }
 
-export default App;
+export default Navbar;
 
 // Previous Navbar for deletion later -LE
 
@@ -95,34 +95,3 @@ export default App;
 
 // revised ant design Navbar -LE
 // includes drawer but can delete if necessary -LE
-=======
-// Navbar
-import React, { useState } from "react";
-
-const Navbar = () => {
-    const [isOpen, setIsOpen] = useState(false);
-    
-    const toggleMenu = () => {
-    setIsOpen(!isOpen);
-    };
-    
-    return (
-        <nav className="navbar">
-            <div className="logo">Aviation Payments</div>
-            <div className="navbar-links ${isOpen ? 'open' : ''}">
-                <a href="#home">Home</a>
-                <a href="#services">Services</a>
-                <a href="#contact">Contact Us</a>
-                <a href="#about">About Us</a>
-            </div>
-            <div className="menu-icon" onClick={toggleMenu}>
-                <div className=""></div>
-                <div className=""></div>
-                <div className=""></div>
-            </div>
-        </nav>
-    );
-};  
-
-export default Navbar;
->>>>>>> 85f68d16ac73313d8801d6d25f46c2d55a966057
