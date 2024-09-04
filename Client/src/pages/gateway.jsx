1d4cec82c900989dafad3a499ff1d589e2159b87
mbanyea <mbanyea@gmail.com>
2024-09-04 18:33:09 -0400

    new merge

--- Client/src/pages/gateway.jsx
+++ Client/src/pages/gateway.jsx
@@ -1,6 +1,6 @@
 import React, { Routes, Route } from "react";
 import GatewayFeatures from "../components/SolutionsGateway/GatewayFeatures";
-// import FormlessEmbed from "../components/formlessai";
+import FormlessEmbed from "../components/formlessai";
 import GatewayInfo from "../components/SolutionsGateway/GatewayInfo";
 
 function Gateway() {
@@ -10,7 +10,7 @@ function Gateway() {
       <h2>Gateway Services</h2>
     <GatewayInfo />
     <GatewayFeatures />
-    {/* <FormlessEmbed /> */}
+    <FormlessEmbed />
     </div>
   );
 };
