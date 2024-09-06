008be9597e34e4d99028953068e12c6f4fa4dcbb
Dan Sexton <danielsexton1999@gmail.com>
2024-09-05 21:00:29 -0400

    changed endpoints to use a .env for mounting to render

--- Client/src/constants/endpoints.jsx
+++ Client/src/constants/endpoints.jsx
@@ -1,5 +1,6 @@
-export const API_LEAD_SUBMIT = "http://localhost:4000/api/createLead";
-export const API_USER_CONTROL = "http://localhost:4000/auth";
-export const API_API_KEY = "http://localhost:4000/apiKey";
-export const API_LEAD_Display= "http://localhost:4000/api"
-export const API_BLOG = "http://localhost:4000/blog";
+let server = import.meta.env.VITE_API_BASE;
+export const API_LEAD_SUBMIT = `${server}/api/createLead`;
+export const API_USER_CONTROL = `${server}/auth`;
+export const API_API_KEY = `${server}/apiKey`;
+export const API_LEAD_Display = `${server}/api`;
+export const API_BLOG = `${server}/blog`;
