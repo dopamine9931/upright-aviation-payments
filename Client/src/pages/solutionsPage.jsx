dc554d63d57bcf804bed1f8d130705a4bf6c23b3
lucemery <lucemery316@gmail.com>
2024-09-10 19:34:10 -0500

    spacing between solutions page elements

--- Client/src/pages/solutionsPage.jsx
+++ Client/src/pages/solutionsPage.jsx
@@ -4,16 +4,27 @@ import Gateway from "../pages/gateway.jsx";
 import Consulting from "../pages/consulting.jsx";
 import OtherSolutions from "../components/othersolutions.jsx";
 
+const spacingStyle = {
+  height: "150px",
+  width: "",
+  padding: "",
+};
+
 function solutionsPage() {
   return (
     <>
       <Consulting />
+      <div style={spacingStyle} />
 
       <MerchantServices />
+      <div style={spacingStyle} />
 
       <OtherSolutions />
+      <div style={spacingStyle} />
       
       <Gateway />
+      <div style={spacingStyle} />
+      
     </>
   );
 }
