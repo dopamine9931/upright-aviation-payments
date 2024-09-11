8213c92fdf966eae92546b08c8e185a6ae363e5f
lucemery <lucemery316@gmail.com>
2024-09-11 18:07:00 -0500

    solutionsPage spacing

--- Client/src/pages/solutionsPage.jsx
+++ Client/src/pages/solutionsPage.jsx
@@ -1,32 +1,37 @@
 import React, { Routes, Route } from "react";
+import { Layout, Space } from "antd"; 
 import MerchantServices from "../pages/merchantservices.jsx";
 import Gateway from "../pages/gateway.jsx";
 import Consulting from "../pages/consulting.jsx";
 import OtherSolutions from "../components/othersolutions.jsx";
 
-const spacingStyle = {
-  height: "150px",
-  width: "",
-  padding: "",
-};
+
+const { Content } = Layout;
+
+//const spacingStyle = {
+//  height: "150px"
+ // padding: "100px"
+//};
 
 function solutionsPage() {
-  return (
-    <>
+return (
+  <Layout>
+    <Content
+      className="site-layout"
+      style={{ padding: "0 50px", marginTop: 60 }}>
+    <Space
+      direction="vertical"
+      size="large"
+      style={{ display: "flex", background: "#000000" }}>
       <Consulting />
-      <div style={spacingStyle} />
-
       <MerchantServices />
-      <div style={spacingStyle} />
-
       <OtherSolutions />
-      <div style={spacingStyle} />
-      
       <Gateway />
-      <div style={spacingStyle} />
-      
-    </>
-  );
+    </Space>
+  </Content>
+</Layout>
+);
 }
+
 
 export default solutionsPage;
